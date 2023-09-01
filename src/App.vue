3ccca18490cfdf94f4341b94f46dca8d85df29a0
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-09-01 18:40:54 +0200

    Added dynamic create/data fetching first section

--- src/App.vue
+++ src/App.vue
@@ -29,50 +29,17 @@ export default {
     <TheHeader></TheHeader>
     <main>
         <section>
-            <MainSeparator :title="store.firstMainSeparator.title"></MainSeparator>
+            <MainSeparator :title="store.separatorsData.firstSeparator.title"></MainSeparator>
             <div class="container main-container">
                 <div class="row row-cols-4 g-4">
-                    <div class="col">
+                    <div class="col" v-for="card in store.cardsData.firstCard">
                         <div class="first-card card border-0 text-center">
                             <div class="card-body">
                                 <div class="icon-container d-flex justify-content-center align-items-center my-3">
-                                    <i class="fa-regular fa-building"></i>
+                                    <i :class="`${card.icon}`"></i>
                                 </div>                        
-                                <h2 class="card-title">Buildings</h2>
-                                <p class="card-text">Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="first-card card border-0 text-center">
-                            <div class="card-body">
-                                <div class="icon-container d-flex justify-content-center align-items-center my-3">
-                                    <i class="fa-regular fa-building"></i>
-                                </div>                        
-                                <h2 class="card-title">Buildings</h2>
-                                <p class="card-text">Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="first-card card border-0 text-center">
-                            <div class="card-body">
-                                <div class="icon-container d-flex justify-content-center align-items-center my-3">
-                                    <i class="fa-regular fa-building"></i>
-                                </div>                        
-                                <h2 class="card-title">Buildings</h2>
-                                <p class="card-text">Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="first-card card border-0 text-center">
-                            <div class="card-body">
-                                <div class="icon-container d-flex justify-content-center align-items-center my-3">
-                                    <i class="fa-regular fa-building"></i>
-                                </div>                        
-                                <h2 class="card-title">Buildings</h2>
-                                <p class="card-text">Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum.</p>
+                                <h2 class="card-title">{{ card.title }}</h2>
+                                <p class="card-text">{{ card.text }}</p>
                             </div>
                         </div>
                     </div>
--- src/store.js
+++ src/store.js
@@ -10,9 +10,36 @@ export const store = reactive ({
         "ARTICLES"
     ],
 
-    firstMainSeparator: {
-        title: "Specialists In Modern Construction",
-        text: ""
+    cardsData: {
+        firstCard: [
+            {
+                icon: "fa-regular fa-building",
+                title: "Buildings",
+                text: "Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum."
+            },
+            {
+                icon: "fa-solid fa-rotate",
+                title: "Renovate",
+                text: "Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum."
+            },
+            {
+                icon: "fa-solid fa-house",
+                title: "Consruct",
+                text: "Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum."
+            },
+            {
+                icon: "fa-solid fa-truck",
+                title: "Exclusive",
+                text: "Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum."
+            },
+        ]
+    },
+
+    separatorsData: {
+        firstSeparator: {
+            title: "Specialists In Modern Construction",
+            text: ""
+        },
     }
 
 })
