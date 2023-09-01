c3c22eaaea01fe46dfef288e64a3a8a9a5455708
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-09-01 16:13:58 +0200

    Working on separator component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 
 import TheTopNav from './components/TheTopNav.vue';
 import TheHeader from './components/TheHeader.vue';
+import MainSeparator from './components/MainSeparator.vue';
 
 export default {
 
     components: {
         TheTopNav,
-        TheHeader
+        TheHeader,
+        MainSeparator,
     }
 
 }
@@ -17,6 +19,7 @@ export default {
 <template>
     <TheTopNav></TheTopNav>
     <TheHeader></TheHeader>
+    <MainSeparator></MainSeparator>
 </template>
 
 <style>
---
+++ src/components/MainSeparator.vue
@@ -0,0 +1,46 @@
+<script>
+
+export default {
+
+    props: {
+        title: {
+            type: String,
+            // required: true
+        },
+        text: {
+            type: String,
+            // required: true,
+            // default: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam."
+        }
+    }
+
+}
+
+</script>
+
+<template>
+    <div class="container text-center">
+        <h2 class="py-4">Specialists In Modern Constructions</h2>
+        <div class="line-separator m-auto"></div>
+        <p class="py-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam.</p>
+    </div>
+</template>
+
+<style scoped>
+
+    .container {
+        padding: 100px 230px 100px 230px;
+        margin: auto;
+    }
+
+    .line-separator {
+        width: 230px;
+        border: 1px solid var(--custom-color-2);
+    }
+
+    p {
+        font-size: 0.9rem;
+        line-height: 2rem;
+    }
+
+</style>
